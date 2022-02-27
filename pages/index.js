a6e40e035e6357aa410a0e202169053d4f3b4d2c
Ander Suarez <me@andersuarez.com>
2022-02-27 12:19:54 +0100

    Update content

--- pages/index.js
+++ pages/index.js
@@ -6,28 +6,23 @@ function HomePage() {
       <div className={styles.mainSectionContent}>
         <h1 className={styles.title}>Ander Suárez Martínez</h1>
         <h2 className={styles.subtitle}>
-          Product and integrations at{' '}
+          Product development and integrations at{' '}
           <a className={styles.inlineLink} href="https://www.jointriple.com">
             Triple
           </a>
-          . I build products and websites.
+          . I like to develop websites and build nice products.
         </h2>
         <p className={styles.text}>
           Previously, Senior Frontend Developer at{' '}
           <a className={styles.inlineLink} href="https://www.unbound.com">
             Unbound
           </a>
-          . I also led frontend development at{' '}
-          <a className={styles.inlineLink} href="https://www.jinnapp.com">
-            Jinn
-          </a>
-          . I co-founded{' '}
+          . I also led frontend development at Jinn . I co-founded{' '}
           <a className={styles.inlineLink} href="http://perpetuall.net/">
             Perpetuall
           </a>
-          . I was born in Bilbao, and moved to the UK looking for new
-          challenges. Now I work remotely from Bilbao. I like music & arts. I
-          play guitar for{' '}
+          . I moved to the UK looking for new challenges. Now I work remotely
+          back from Bilbao. I like music & arts. I play guitar for{' '}
           <a className={styles.inlineLink} href="http://walnut.bandcamp.com">
             Walnut
           </a>
